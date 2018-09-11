77ccbc4a4c7ee4cec33b073826514213835da49d
ue4prog <34624590+ue4prog@users.noreply.github.com>
2018-09-12 00:38:45 +0300

    easy math guys

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+    let counter = 0
+    while(number > 0) {
+        number = Math.floor(number  / 5);
+        counter += number;
+    }
+    return counter
 }
